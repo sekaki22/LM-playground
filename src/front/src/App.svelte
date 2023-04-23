9041974fe5bceb069cb0c1be2e0fb68a19d9f691
Sven Hendrikx <sven_hendrikx@hotmail.com>
2023-04-23 00:13:03 +0000

    fix indentation

--- src/front/src/App.svelte
+++ src/front/src/App.svelte
@@ -1,115 +1,112 @@
 <script>
-  import { onMount } from 'svelte';
-  import TemplateChooser from './TemplateChooser.svelte';
-  import ModelChooser from './ModelChooser.svelte';
-  import ChatWindow from './ChatWindow.svelte';
-  let textInput = '';
-  let selectedTemplate = '';
-  let selectedModel = '';
+    import { onMount } from 'svelte';
+    import TemplateChooser from './TemplateChooser.svelte';
+    import ModelChooser from './ModelChooser.svelte';
+    import ChatWindow from './ChatWindow.svelte';
+    let textInput = '';
+    let selectedTemplate = '';
+    let selectedModel = '';
 
-  const templates = [
-    { id: 1, name: 'Template 1', content: 'Sample content for Template 1' },
-    { id: 2, name: 'Template 2', content: 'Sample content for Template 2' },
-  ];
+    const templates = [
+        { id: 1, name: 'Template 1', content: 'Sample content for Template 1' },
+        { id: 2, name: 'Template 2', content: 'Sample content for Template 2' },
+    ];
 
-  const models = ['databricks/dolly-v2-3b', 'EleutherAI/pythia-2.8b'];
+    const models = ['databricks/dolly-v2-3b', 'EleutherAI/pythia-2.8b'];
 
-  let chatHistory = [];
+    let chatHistory = [];
 
 
-  let modelStatus = '';
+    let modelStatus = '';
 
-  const selectModel = async () => {
-    modelStatus = 'loading';
-    try {
-      const response = await fetch('/model', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ model: selectedModel }),
-      });
+    const selectModel = async () => {
+        modelStatus = 'loading';
+        try {
+            const response = await fetch('/model', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ model: selectedModel }),
+            });
 
-      if (response.ok) {
-        modelStatus = 'success';
-      } else {
-        modelStatus = 'error';
-      }
-    } catch (error) {
-      modelStatus = 'error';
-    }
-  };
+            if (response.ok) {
+                modelStatus = 'success';
+            } else {
+                modelStatus = 'error';
+            }
+        } catch (error) {
+            modelStatus = 'error';
+        }
+    };
 
-  const submitForm = async (hist) => {
-      console.log(textInput)
-    chatHistory = [...chatHistory, { type: 'input', content: textInput }];
-    const response = await fetch(`/predict?prompt=${encodeURIComponent(textInput)}`, {
-      method: 'GET',
-    });
-    const data = await response.text();
+    const submitForm = async (hist) => {
+        console.log(textInput)
+        chatHistory = [...chatHistory, { type: 'input', content: textInput }];
+        const response = await fetch(`/predict?prompt=${encodeURIComponent(textInput)}`, {
+            method: 'GET',
+        });
+        const data = await response.text();
 
-    chatHistory = [...chatHistory, { type: 'output', content: data }];
-    console.log(chatHistory)
-  };
+        chatHistory = [...chatHistory, { type: 'output', content: data }];
+        console.log(chatHistory)
+    };
 
-  const populateTextbox = () => {
-    const template = templates.find((t) => t.name === selectedTemplate);
-    textInput = template ? template.content : '';
-  };
+    const populateTextbox = () => {
+        const template = templates.find((t) => t.name === selectedTemplate);
+        textInput = template ? template.content : '';
+    };
 </script>
 <div class="layout">
-  <div class="main-content">
-    <main>
-        <h1>Svelte App</h1>
-
-        <ChatWindow
-        {chatHistory}
-        bind:textInput
-        on:submitForm="{submitForm}"
-      />
-
-    </main>
-  </div>
-  <div class="right-column">
-    <ModelChooser
-      {models}
-      bind:selectedModel
-      on:modelChange="{(e) => selectModel(e.detail)}"
-    />
-    <TemplateChooser
-        {templates}
-        bind:selectedTemplate
-        on:templateChange="{populateTextbox}"
-      />
-  </div>
+    <div class="main-content">
+        <main>
+            <ChatWindow
+                {chatHistory}
+                bind:textInput
+                on:submitForm="{submitForm}"
+            />
+        </main>
+    </div>
+    <div class="right-column">
+        <ModelChooser
+            {models}
+            bind:selectedModel
+            on:modelChange="{(e) => selectModel(e.detail)}"
+        />
+        <TemplateChooser
+            {templates}
+            bind:selectedTemplate
+            on:templateChange="{populateTextbox}"
+        />
+    </div>
 </div>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 500px;
-    margin: 0 auto;
-    font-family: 'Roboto', sans-serif;
-  }
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 500px;
+        margin: 0 auto;
+        font-family: 'Roboto', sans-serif;
+    }
 
-  h1 {
-    color: #59656f;
-  }
+    h1 {
+        color: #59656f;
+    }
 
-  .layout {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: flex-start;
+    .layout {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
     }
 
     .main-content {
-       flex: 1;
-       margin-right: 2rem;
-       border-right: 1px solid #ccc;
-  }
+        flex: 1;
+        margin-right: 2rem;
+        border-right: 1px solid #ccc;
+    }
 
     .right-column {
-      flex: 0 0 200px; /* You can adjust the width of the right column here */
+        flex: 0 0 200px; /* You can adjust the width of the right column here */
     }
 
 
--- src/front/src/TemplateChooser.svelte
+++ src/front/src/TemplateChooser.svelte
@@ -1,33 +1,33 @@
 <script>
-  export let templates = [];
-  export let selectedTemplate = '';
-  export let onTemplateChange = () => {};
+    export let templates = [];
+    export let selectedTemplate = '';
+    export let onTemplateChange = () => {};
 
-  const populateTextbox = () => {
-    onTemplateChange(selectedTemplate);
-  };
+    const populateTextbox = () => {
+        onTemplateChange(selectedTemplate);
+    };
 </script>
 
 <div>
-  <label for="templates">Choose a template:</label>
-  <select bind:value="{selectedTemplate}" id="templates" on:change="{populateTextbox}">
-    <option value="">Select a template</option>
-    {#each templates as template}
-      <option value="{template.name}">{template.name}</option>
-    {/each}
-  </select>
+    <label for="templates">Choose a template:</label>
+    <select bind:value="{selectedTemplate}" id="templates" on:change="{populateTextbox}">
+        <option value="">Select a template</option>
+        {#each templates as template}
+            <option value="{template.name}">{template.name}</option>
+        {/each}
+    </select>
 </div>
 
 
 <style>
-  select {
-    width: 100%;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    padding: 0.5em;
-  }
-  label {
-    display: block;
-    margin-bottom: 0.5em;
-  }
+    select {
+        width: 100%;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 0.5em;
+    }
+    label {
+        display: block;
+        margin-bottom: 0.5em;
+    }
 </style>
